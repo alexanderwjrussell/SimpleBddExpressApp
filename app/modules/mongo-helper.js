23e21e777fb372c616b0463f628af98506c985c4
Alexander Russell <awjrussell@live.co.uk>
2018-06-28 16:25:26 +0100

    Refactor

--- app/modules/mongo-helper.js
+++ app/modules/mongo-helper.js
@@ -1,37 +1,52 @@
 module.exports = {
-    uri: '',
-    host: '',
-    connected: false,
-    connect(mongoose, uri) {
-      mongoose.Promise = global.Promise;
-  
-      mongoose.connection.on('connected', () => {
-        console.log('Connected to Mongo');
-        this.uri = `${mongoose.connection.host}:${mongoose.connection.port}`;
-        this.connected = true;
-      });
-      //note: reconnect only works if mongod downtime is less then 10 sec; it's in mongoose config
-      mongoose.connection.on('reconnected', () => {
-        console.log('Reconnected to Mongo');
-        this.connected = true;
-      });
-      mongoose.connection.on('error', e => {
-        console.log(e);
-        console.log('Mongo error!');
-        this.connected = false;
-      });
-      mongoose.connection.on('disconnected', () => {
-        console.log('Mongo disconnected!');
-        this.connected = false;
-      });
-  
-      return mongoose.connect(uri);
-    },
-    isConnected() {
-      return this.connected;
-    },
-    getUri() {
-      return this.uri;
-    },
-  };
+  uri: '',
+  host: '',
+  connected: false,
+  connect(mongoose, uri) {
+    mongoose.Promise = global.Promise;
+
+    mongoConnect(mongoose);
+    mongoReconnect(mongoose);
+    mongoError(mongoose);
+    mongoDisconnected(mongoose);
+
+    return mongoose.connect(uri);
+  },
+  isConnected() {
+    return this.connected;
+  },
+  getUri() {
+    return this.uri;
+  },
+};
+
+function mongoConnect(mongoose) {
+  mongoose.connection.on('connected', () => {
+    console.log('Connected to Mongo');
+    this.uri = `${mongoose.connection.host}:${mongoose.connection.port}`;
+    this.connected = true;
+  });
+}
+
+function mongoReconnect(mongoose) {
+  mongoose.connection.on('reconnected', () => {
+    console.log('Reconnected to Mongo');
+    this.connected = true;
+  });
+}
+
+function mongoError(mongoose) {
+  mongoose.connection.on('error', e => {
+    console.log(e);
+    console.log('Mongo error!');
+    this.connected = false;
+  });
+}
+
+function mongoDisconnected(mongoose) {
+  mongoose.connection.on('disconnected', () => {
+    console.log('Mongo disconnected!');
+    this.connected = false;
+  });
+}
   
--- app/routes/healthRoutes.js
+++ app/routes/healthRoutes.js
@@ -2,12 +2,9 @@
 
 const express = require('express');
 const router = express.Router();
-
 const mongoose = require('mongoose');
 const mongoHelper = require('../modules/mongo-helper');
-
 const mongoUri = process.env.MONGO || 'mongodb://localhost:27017/nameDB';
-
 const mongoConnection = mongoHelper.connect(mongoose, mongoUri);
 
 router.get('/private/ping', (req, res) => {
